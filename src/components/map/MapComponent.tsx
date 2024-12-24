6a03981bada6f7d6bb280472b1df15b11cd30215
minjun <qkralswns1998@gmail.com>
2024-12-24 17:09:00 +0900

    style: 메인 페이지의 지도 ui에 반응형 도입.

--- src/components/map/MapComponent.tsx
+++ src/components/map/MapComponent.tsx
@@ -5,31 +5,33 @@ import { Camping } from '@/types/Camping';
 
 export type MapComponentProps = {
   radiusCampList: Pick<
-  Camping,
-  | 'contentId'
-  | 'firstImageUrl'
-  | 'facltNm'
-  | 'addr1'
-  | 'induty'
-  | 'mapY'
-  | 'mapX'
-  | 'tel'
->[]
+    Camping,
+    | 'contentId'
+    | 'firstImageUrl'
+    | 'facltNm'
+    | 'addr1'
+    | 'induty'
+    | 'mapY'
+    | 'mapX'
+    | 'tel'
+  >[];
   geoData: coords | null;
 };
 
 const MapComponent = ({ radiusCampList, geoData }: MapComponentProps) => {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center">
-        <p className="p-3">캠핑장 찾기</p>
-        <div className="w-[1200px]">
-          <main className="flex flex-col items-center w-[1200px] h-[600px]">
-            <KakaoMap radiusCampList={radiusCampList} geoData={geoData} />
-          </main>
-        </div>
+    <div className="flex flex-col items-center justify-center mx-auto pt-10">
+      <p className="font-extrabold text-4xl py-[60px] text-center">
+        캠핑장 찾기
+      </p>
+
+      {/* 화면 크기에 따라 유동적으로 크기가 줄어드는 div */}
+      <div className="w-full max-w-[1920px] h-[600px] aspect-[16/5] transition-all duration-500 ease-in-out">
+        <main className="flex flex-col items-center w-full h-full">
+          <KakaoMap radiusCampList={radiusCampList} geoData={geoData} />
+        </main>
       </div>
-    </>
+    </div>
   );
 };
 
