02b783db00fb3bc5333365653c9eedd92ced44dd
chay140 <chaihyunpark140@gmail.com>
2024-12-25 02:16:07 +0900

    style: 정적 지도 이미지 모바일 환경에서의  반응형 추가

--- src/components/detail/PlaceDetail.tsx
+++ src/components/detail/PlaceDetail.tsx
@@ -2,6 +2,7 @@ import { PawPrint, Phone } from 'lucide-react';
 import LikeButton from './LikeButton';
 import { Camping } from '@/types/Camping';
 import DetailMap from './DetailMap';
+import Image from 'next/image';
 
 type PlaceDetailProps = {
   details: Camping;
@@ -12,9 +13,7 @@ const PlaceDetail = ({ details }: PlaceDetailProps) => {
     <div className="flex flex-col items-center sm:flex-row gap-8 border justify-between align-center border-black rounded-xl px-10 py-8 mb-6">
       {/* 장소 상세 정보 */}
       <div className="flex flex-col justify-center">
-        <h2 className="text-2xl font-semibold mb-4">
-          {details.facltNm}
-        </h2>
+        <h2 className="text-2xl font-semibold mb-4">{details.facltNm}</h2>
         <p className="mb-2 text-gray-500">{details.addr1}</p>
         <p className="mb-2 text-gray-500">
           부대 시설: {details.sbrsCl || '해당 사항 없음'}
@@ -24,7 +23,7 @@ const PlaceDetail = ({ details }: PlaceDetailProps) => {
         </p>
         <p className="mb-2 mt-8 text-gray-500 flex flex-row gap-2">
           <PawPrint />
-          {` `}애완 동물 출입 여부: {details.animalCmgCl ? '가능' : '불가능'}
+          {` `}애완 동물: {details.animalCmgCl ? '출입 가능' : '출입 불가능'}
         </p>
         <div className="flex flex-row justify-between mb-2 mt-4 items-center">
           <p className=" text-gray-500 flex flex-row gap-2">
@@ -35,7 +34,25 @@ const PlaceDetail = ({ details }: PlaceDetailProps) => {
       </div>
 
       {/* 지도 부분 */}
-      <div className="justify-items-center items-center mobile:max-w-[200px] aspect-square sm:max-w-[350px] overflow-hidden">
+      <div className="justify-items-center align-items-center mobile:max-w-[200px] sm:aspect-square sm:max-w-[340px] overflow-hidden rounded-lg">
+        {/* {details.firstImageUrl ? (
+          <Image
+            src={details.firstImageUrl}
+            alt={details.facltNm}
+            width={400}
+            height={250}
+            className="h-[350px]"
+          />
+        ) : (
+          <DetailMap
+            latitude={parseFloat(details.mapY)}
+            longitude={parseFloat(details.mapX)}
+            level={3}
+            width="350px"
+            height="350px"
+          />
+        )} */}
+
         <DetailMap
           latitude={parseFloat(details.mapY)}
           longitude={parseFloat(details.mapX)}
