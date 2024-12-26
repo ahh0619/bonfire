31dbbca7d86ce5e999b9074c32ac2f78d03a9d12
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-12-26 15:18:14 +0900

    fix: redirectTo: window.origin을 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -32,7 +32,7 @@ const LoginPage = () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        // redirectTo: window.origin + '/auth/callback',
+        redirectTo: window.origin + '/auth/callback',
         queryParams: {
           access_type: 'offline',
           prompt: 'consent',
