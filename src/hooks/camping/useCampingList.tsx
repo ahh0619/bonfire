c05f5aaf153056e610ca7be06f054f6db49829df
minjun <qkralswns1998@gmail.com>
2024-12-26 20:58:40 +0900

    feat: 위치 정보 받아오지 못 할 시에 알림 설정. style: 스윗한 얼럿 적용.

--- src/hooks/camping/useCampingList.tsx
+++ src/hooks/camping/useCampingList.tsx
@@ -5,14 +5,33 @@ import { useEffect, useState } from 'react';
 
 const useCampingList = () => {
   const [geoData, setGeoData] = useState<coords | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const Swal = require('sweetalert2');
+
   useEffect(() => {
     const getGeoData = async () => {
-      navigator.geolocation.getCurrentPosition((res) => {
-        setGeoData(res.coords);
-      });
+      try {
+        const position = await new Promise<GeolocationPosition>(
+          (resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+          },
+        );
+        setGeoData(position.coords); // 위치 정보 저장
+      } catch (error) {
+        console.error('Error fetching location:', error);
+        Swal.fire({
+          icon: 'error',
+          text: '위치 정보를 가져올 수 없습니다. 설정에서 위치 권한을 허용해주세요.',
+          confirmButtonColor: "#FD470E",
+        });
+      } finally {
+        setIsLoading(false); // 로딩 상태 업데이트
+      }
     };
+
     getGeoData();
-  }, []);
+  }, []); // 컴포넌트가 마운트될 때 실행
+
   const {
     data: radiusCampData,
     isPending: isCampListPending,
--- src/utils/map/facilSearch.ts
+++ src/utils/map/facilSearch.ts
@@ -4,7 +4,7 @@ export const facilSearch = (
   lat: number,
   lon: number,
   setFacilSearchResult: Dispatch<kakao.maps.services.PlacesSearchResult | []>,
-  code: `${kakao.maps.CategoryCode}`
+  code: `${kakao.maps.CategoryCode}`,
 ) => {
   window.kakao.maps.load(() => {
     const places = new window.kakao.maps.services.Places();
@@ -13,14 +13,19 @@ export const facilSearch = (
       status: kakao.maps.services.Status,
       _: kakao.maps.Pagination,
     ) => {
-      console.log(result, status);
+      const Swal = require('sweetalert2');
       if (status === 'OK') {
         console.log('검색 결과:', result);
         console.log(result);
         setFacilSearchResult(result);
       } else {
         if (status === 'ZERO_RESULT') {
-          alert('검색 결과가 존재하지 않습니다.');
+          // alert('검색 결과가 존재하지 않습니다.');
+          Swal.fire({
+            icon: 'error',
+            text: '검색 결과가 존재하지 않습니다.',
+            confirmButtonColor: '#FD470E',
+          });
         }
         console.error('검색 실패:', status);
       }
