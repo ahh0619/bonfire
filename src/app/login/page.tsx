875e1df337ab3631aab78fcbd2017ad24bf6ff5c
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-12-26 15:49:18 +0900

    feat: next_public_url 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -32,7 +32,7 @@ const LoginPage = () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.origin + '/auth/callback',
+        redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`,
         queryParams: {
           access_type: 'offline',
           prompt: 'consent',
