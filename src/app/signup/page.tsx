5292443bd8278d96f09d0c3c8f00b00052ab79b8
ahh0619 <ahh900619@gmail.com>
2024-12-27 19:46:32 +0900

    feat : 로그인/회원가입 메타데이터 설정

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,10 @@
 import SignUpPage from '@/components/login/SignUpPage';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'BonFire - 회원가입 ',
+  description: 'BonFire - 회원가입 페이지입니다.',
+};
 
 const Page = () => {
   return <SignUpPage />;
