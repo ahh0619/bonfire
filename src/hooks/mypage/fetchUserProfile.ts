e56f451cf43fb98ab62279356719adde0b62d5d1
ahh0619 <ahh900619@gmail.com>
2024-12-27 15:12:16 +0900

    chore : 충돌해결

--- src/hooks/mypage/fetchUserProfile.ts
+++ src/hooks/mypage/fetchUserProfile.ts
@@ -2,7 +2,7 @@ import { getUser } from '@/app/login/actions';
 
 const fetchUserProfile = async () => {
   const data = await getUser();
-  return data[0]; // 인증된 유저의 프로필 반환
+  return data![0]; // 인증된 유저의 프로필 반환
 };
 
 export default fetchUserProfile;
