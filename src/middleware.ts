760ec760ac2b53214f8e70c576f69d5c6ab61cb1
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-12-28 00:03:23 +0900

    chore: 불필요한 콘솔 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from '@/utils/supabase/middleware';
 
 export const middleware = async (request: NextRequest) => {
-  console.log('미들웨어가 동작한다');
   return await updateSession(request);
 };
 
