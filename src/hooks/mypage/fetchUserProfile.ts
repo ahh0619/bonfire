17c2385a9484049b11ed6121576dc45c28d29d17
ahh0619 <ahh900619@gmail.com>
2024-12-27 15:10:29 +0900

    refactor : 유저정보 로드함수 분리

---
+++ src/hooks/mypage/fetchUserProfile.ts
@@ -0,0 +1,8 @@
+import { getUser } from '@/app/login/actions';
+
+const fetchUserProfile = async () => {
+  const data = await getUser();
+  return data[0]; // 인증된 유저의 프로필 반환
+};
+
+export default fetchUserProfile;
