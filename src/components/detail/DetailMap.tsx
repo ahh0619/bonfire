35337c46bfbcdcef5d7e37f74a3a22e062ad8892
chay140 <chaihyunpark140@gmail.com>
2024-12-29 22:18:14 +0900

    chore: detail map props 전달 바꿈

--- src/components/detail/DetailMap.tsx
+++ src/components/detail/DetailMap.tsx
@@ -4,17 +4,24 @@ import Script from 'next/script';
 import { useState } from 'react';
 import { StaticMap } from 'react-kakao-maps-sdk';
 import useSdkLoad from '@/hooks/map/useSdkLoad';
-import { Camping } from '@/types/Camping';
 
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`;
 
 type DetailMapProps = {
-  details: Camping;
-  width: string;
-  height: string;
+  latitude: number;
+  longitude: number;
+  level?: number;
+  width?: string;
+  height?: string;
 };
 
-const DetailMap = ({ details, width, height }: DetailMapProps) => {
+const DetailMap = ({
+  latitude,
+  longitude,
+  level = 3,
+  width = '100%',
+  height = '300px',
+}: DetailMapProps) => {
   const [isSdkLoaded, setIsSdkLoaded] = useState(false);
   useSdkLoad(setIsSdkLoaded);
 
@@ -23,17 +30,14 @@ const DetailMap = ({ details, width, height }: DetailMapProps) => {
       <Script src={KAKAO_SDK_URL} strategy="afterInteractive" />
       {isSdkLoaded ? (
         <StaticMap
-          center={{ lat: Number(details.mapY), lng: Number(details.mapY) }}
+          center={{ lat: latitude, lng: longitude }}
           style={{
             // 지도의 크기
             width,
             height,
           }}
-          level={3}
-          marker={{
-            position: { lat: Number(details.mapY), lng: Number(details.mapY) },
-          }}
-          id="detailmap"
+          level={level}
+          marker={{ position: { lat: latitude, lng: longitude } }}
         />
       ) : (
         <div
